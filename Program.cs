c8c472b362edb01749f0cc284afe2153824f9629
Yusuf <yusufsonmez951@gmail.com>
2023-07-03 00:12:51 +0300

    7896540123134578971 sayısı için deneme yapıldı sonuç pozitif

--- Program.cs
+++ Program.cs
@@ -10,6 +10,28 @@ namespace CollatzConjecture
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(CollatzConjecture(7896540123134578971));
         }
+        public static int CollatzConjecture(decimal number)
+        {
+            var iter = 0;
+            while(number != 1)
+            {
+                if(number % 2 == 0)
+                {
+                    number /= 2;
+                    iter++;
+                    Console.WriteLine(number);
+                }
+                else
+                {
+                    number = (3 * number) + 1;
+                    iter++;
+                    Console.WriteLine(number);
+                }
+            }
+            return iter;
+        }
+
     }
 }
